ccff12f5376fb1327a0614dd4b85220d95a93a25
lrm2000 <hirok7433@gmail.com>
2024-03-04 16:33:30 -0600

    added frontend and steamcard component

---
+++ frontend/src/components/landing_page.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Flex,
+    Box,
+    Avatar,
+    Heading
+} from "@chakra-ui/react";
+
+const LandingPage = () => {
+    return (
+        <Box bg="#171a21" minH="100vh">
+        <Flex justify="center" align="center" h="100vh" style={{
+            margin: 'auto'
+        }}>
+            <Card bg='#1b2838'>
+            <CardHeader color='azure'>Steam Analysis</CardHeader>
+            <CardBody>
+                <Flex justifyContent="space-between" alignItems="center">
+                <Avatar name="ito" src="../../public/ito_pfp.png" />
+                <Heading color="azure" size="md">ito</Heading>
+                </Flex>
+            </CardBody>
+            </Card>
+        </Flex>
+        </Box>
+    );
+};
+
+export default LandingPage;
